5bdc0dceb157cfe9916c6dd4616f7e86ab7224ad
MadiKABA <madikaba2004@gmail.com>
2021-12-22 13:39:06 +0000

    controle  du nombre de caractere saisie dans le champs biographie

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-let formApprenant=document.querySelector("#form-apprenant");
-let nom=document.querySelector("#nom")
-let prenom=document.querySelector("#prenom")
-let biographie=document.querySelector("#biographie")
+const formApprenant=document.querySelector("#form-apprenant");
+const nom=document.querySelector("#nom")
+const prenom=document.querySelector("#prenom")
+const biographie=document.querySelector("#biographie")
+
+
 
 let span=document.createElement("span")
 formApprenant.addEventListener("submit", (e)=>{
@@ -29,3 +31,35 @@ function validationFOrmulaire(input,msgerreur,span)
     input.insertAdjacentElement("beforebegin",span)
     input.style.border="1px solid red"
 }
+
+
+biographie.addEventListener("input", (event)=>{
+    const maxcaractere=130
+    const contenuSaisie=biographie.value
+    const longueurSaisi=contenuSaisie.length
+    const rest= maxcaractere-longueurSaisi
+
+    //actualiser le dom pour afficher le nombre
+
+    const paragrapheLimiteTexte=document.querySelector("#paragraphe-limite-texte")
+    const spanNombreCaracteres=document.querySelector("#span-nombre-caracteres")
+    const testResntant=document.querySelector("#testResntant")
+    spanNombreCaracteres.textContent=longueurSaisi
+    testResntant.textContent="il vous reste:  "+rest+" caractere a saisir"
+
+    if(rest<=10)
+    {
+        paragrapheLimiteTexte.style.color="yellow"
+    }
+    if(rest<0)
+    {
+        paragrapheLimiteTexte.style.color="red"
+        btnAjouter.disabled=true
+    }
+    if(rest==10)
+    {
+        paragrapheLimiteTexte.style.color="black"
+        btnAjouter.disabled=false
+    }
+    
+})
